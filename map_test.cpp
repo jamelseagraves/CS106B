c9e70bc433b963298e89f5b2128f7d7698d41b78
Jamel Seagraves <jseagra5@ford.com>
2015-01-06 18:42:11 -0800

    added to code

--- map_test.cpp
+++ map_test.cpp
@@ -1,21 +1,70 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include <map>
 #include <vector>
 using namespace std;
 
+int sequence_length = 0;
+ifstream ifs;
+
+void openFile(const char* filename);
+void readFile();
+void sortData(vector<char> v);
+
 int main() {
+string input;
+while (sequence_length == 0) {
+cout << "Enter character sequence length (must be a positive integer): ";
+getline(cin, input);
+stringstream inputStream(input);
+if (!(inputStream >> sequence_length)) {
+sequence_length = 0;
+}
+}
+
+string fileName;
+cout << "Enter file name (including .ext): ";
+getline(cin, fileName);
+openFile(fileName.c_str());
+return 0;
+}
+
+void openFile(const char* filename) {
+ifs.open(filename);
+if (ifs.fail()) {
+ifs.clear();
+string fileName;
+cout << "Could not open file. Please enter valid file name (including .ext): ";
+getline(cin, fileName);
+openFile(fileName.c_str());
+} else {
+readFile();
+}
+}
+
+void readFile() {
+char c;
+vector<char> input_chars;
+vector<char>::iterator it;
+while (ifs >> c) {
+it = input_chars.end();
+it = input_chars.insert(it, c);
+}
+sortData(input_chars);
+}
+
+void sortData(vector<char> v) {
 map<string,map<string,int> m1;
 map<string,int> m2;
-vector<char> input_chars;
-int sequence_length;
 int char_count = 0;
 string sequence;
-for (unsigned int i = 0; i < input_chars; i++) {
+for (unsigned int i = 0; i < v.size(); i++) {
 if (char_count < sequence_length) {
-sequence += input_chars.at(i);
+sequence += v.at(i);
 char_count++;
 } else {
-string new_sequence = input_chars.at(i);
+string new_sequence = v.at(i);
 if(m2[new_sequence]) {
 m2[new_sequence] += 1;
 } else {
@@ -28,7 +77,6 @@ char_count++;
 i -= (sequence_length - 1);
 }
 }
-return 0;
 }
 
 void getMostFrequentSequence(vector<char> v) {
